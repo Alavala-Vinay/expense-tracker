4618e86584f32d55cb88fa65537bc95f74732ca0
Alavala-Vinay <142196470+Alavala-Vinay@users.noreply.github.com>
2025-09-05 19:35:32 +0530

    Update expenseRoutes.js

--- backend/routes/expenseRoutes.js
+++ backend/routes/expenseRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const {addExpense, getAllExpenses, deleteExpense,downloadExpenseExcel} = require('../controllers/expenseController.js');
+const {addExpense, getExpensesByDate, deleteExpense,downloadExpenseExcel} = require('../controllers/expenseController.js');
 
 const { protect } = require('../middleware/authMiddleware.js');
 
 const expenseRouter = express.Router();
 
 expenseRouter.post('/add', protect, addExpense);
-expenseRouter.get('/get', protect, getAllExpenses);
+expenseRouter.get('/get', protect, getExpensesByDate);
 expenseRouter.delete('/:id', protect, deleteExpense);
 expenseRouter.get('/download', protect, downloadExpenseExcel);
 
