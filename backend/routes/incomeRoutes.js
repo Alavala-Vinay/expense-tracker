81c91e982bb90894194bac624ab80ef10688cbec
Alavala-Vinay <142196470+Alavala-Vinay@users.noreply.github.com>
2025-09-05 19:35:09 +0530

    Update incomeRoutes.js

--- backend/routes/incomeRoutes.js
+++ backend/routes/incomeRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const {addIncome, getAllIncomes, deleteIncome,downloadIncomeExcel} = require('../controllers/incomeController.js');
+const {addIncome, getIncomesByDate, deleteIncome,downloadIncomeExcel} = require('../controllers/incomeController.js');
 
 const { protect } = require('../middleware/authMiddleware.js');
 
 const incomeRouter = express.Router();
 
 incomeRouter.post('/add', protect, addIncome);
-incomeRouter.get('/get', protect, getAllIncomes);
+incomeRouter.get('/get', protect, getIncomesByDate);
 incomeRouter.delete('/:id', protect, deleteIncome);
 incomeRouter.get('/download', protect, downloadIncomeExcel);
 
